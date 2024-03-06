f54d6b60f2d8c9f5c8cbda956b043489950b4e1e
aryan <aryansatija2003@gmail.com>
2024-03-06 17:17:30 +0530

    api integration

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -2,8 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-// import Home from './pages/home'
-import Home from './pages/SecondHome'
+import Home from './pages/home'
+// import Home from './pages/SecondHome'
 
 function App() {
   const [count, setCount] = useState(0)
