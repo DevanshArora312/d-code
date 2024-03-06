7f167bffab67a94209731b47cbcf755aaecd8bc6
DevanshArora312 <devansharora312@gmail.com>
2024-03-06 14:21:39 +0530

    basic layout

---
+++ Client/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='w-full h-auto bg-black p-10 text-white'>
+        Footer
+    </div>
+  )
+}
+
+export default Footer
