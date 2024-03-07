2d59f21fda3e80299acff90b6e9ddbf6192aadb6
aryan <aryansatija2003@gmail.com>
2024-03-07 07:46:19 +0530

    text-to-pdf-api

--- Client/src/components/HeadBar.jsx
+++ Client/src/components/HeadBar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { RxHamburgerMenu } from "react-icons/rx";
 const HeadBar = () => {
   return (
-    <div className='w-full h-auto bg-black flex justify-start border-box p-4'>
+    <div className='w-full h-auto bg-white/20 flex justify-start border-box p-4'>
         <RxHamburgerMenu className='text-white text-[30px]'/>
     </div>
   )
